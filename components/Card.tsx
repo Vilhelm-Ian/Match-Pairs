9e88924247f19d3961f9ad0957e97bdbeecf71ce
Jovan <mariothrowsfieball@gmail.com>
2022-07-25 12:16:23 +0200

    refactored the card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,5 +1,5 @@
 import style from "../styles/Card.module.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { element } from "../pages/index";
 import { CardType } from "./Types";
 
@@ -14,13 +14,35 @@ export default function Card(props: {
 	const [isSolved, setIsSolved] = useState(false);
 
 	let { clicked, index, setClicked, value, setCards } = props;
-	let first_clicked_element = clicked[1];
+	let first_clicked_element = useMemo(() => clicked[1], [clicked]);
+
+	function click(temp = 0) {
+		setIsClicked((oldState) => !oldState);
+
+		setClicked((oldState: [element, element]) => {
+			let newState = [...oldState];
+			for (let i = 0; i < newState.length; i++) {
+				if (newState[i].value === "" && temp === 0) {
+					newState[i] = { key: index, value: value };
+					temp += 1;
+				}
+			}
+			return [...newState];
+		});
+	}
 
 	useEffect(() => {
 		for (let i = 0; i < clicked.length; i++) {
 			if (clicked[i].key === index && isClicked) {
-				if (clicked[0].value === clicked[1].value) setIsSolved(true);
-				else setTimeout(() => setIsClicked(!isClicked), 1000);
+				if (clicked[0].value === clicked[1].value) {
+					setCards((oldCards: CardType[]) => {
+						let newState = [...oldCards];
+						newState[index].clicked = true;
+						return newState;
+					});
+
+					setIsSolved(true);
+				} else setTimeout(() => setIsClicked(!isClicked), 1000);
 				setClicked([
 					{ value: "", key: NaN },
 					{ value: "", key: NaN },
@@ -29,34 +51,10 @@ export default function Card(props: {
 		}
 	}, [first_clicked_element]);
 
-	useEffect(
-		(temp = 0) => {
-			if (value) {
-				setCards((oldCards: CardType[]) => {
-					let newState = [...oldCards];
-					newState[index].clicked = isClicked;
-					return newState;
-				});
-			}
-
-			setClicked((oldState: [element, element]) => {
-				let newState = [...oldState];
-				for (let i = 0; i < newState.length; i++) {
-					if (newState[i].value === "" && temp === 0 && isClicked) {
-						newState[i] = { key: index, value: value };
-						temp += 1;
-					}
-				}
-				return [...newState];
-			});
-		},
-		[isClicked]
-	);
-
 	return (
 		<div className={style.card}>
 			<div
-				onClick={() => setIsClicked(true)}
+				onClick={() => click()}
 				className={`${isClicked || isSolved ? style.card_inner_rotate : ""} ${
 					style.card_inner
 				}`}
